6aa2858fb43bcaa8aa62b02d81bdc7d7889e1552
Gihozo23 <christellegihozo23@gmail.com>
2024-08-11 12:56:27 +0200

    fix the naming of variables

--- src/Todos.jsx
+++ src/Todos.jsx
@@ -3,33 +3,28 @@ import React, { useState } from 'react'
 function Todos(props) {
 
     const [checkbox, setCheckbox] = useState(false);
-    const [visible, setVisible] = useState(true);
-    // function handleClick() {
-    //     setVisible(!visible);
-    // }
     function handleChecked() {
         setCheckbox(!checkbox);
         
     }
 
     return (
-      <>
-            {true &&
-           <div
-           index={props.index}
-           className='flex justify-center items-center gap-16 pt-4'>
-           <input
-               onClick={handleChecked} type='checkbox' className='w-4' name='checkbox' />
-           <p
-               className={checkbox ? "line-through" : ""}
-               name="todo" >{props.task}</p>
-           <button 
-               onClick={() => props.handleDelete(props.index)}
-               
-               className='border-2 border-white rounded-full flex justify-center items-center'>
-           <img name="delete" src="/trash.svg" className='w-4' alt="" />
-       </button >
-</div>}  
+        <>
+            <div
+                index={props.index}
+                className='flex justify-center items-center gap-16 pt-4'>
+                <input
+                    onClick={handleChecked} type='checkbox' className='w-4' name='checkbox' />
+                <p
+                    className={checkbox ? "line-through" : ""}
+                    name="todo" >{props.task}</p>
+                <button 
+                    onClick={() => props.handleDelete(props.index)}
+
+                    className='border-2 border-white rounded-full flex justify-center items-center'>
+                <img name="delete" src="/trash.svg" className='w-4' alt="" />
+                </button >
+            </div>  
       </>
   )
 }
