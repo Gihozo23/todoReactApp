9d465816ac3b303f42d7d78ce2bff1df05c24b86
Gihozo23 <christellegihozo23@gmail.com>
2024-08-14 16:11:05 +0200

    chore: rename the Task component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Task from "./Task";
-
+import Dice from "./Dice";
 function App() {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
